e2b151dbcd38ae05c298fb804a5a55c8771ab293
SharkyKZ <sharkykz@gmail.com>
2022-10-14 08:43:31 +0300

    Update build script

--- build/build.php
+++ build/build.php
@@ -4,15 +4,15 @@ require dirname(__DIR__) . '/build-script/script.php';
 
 (
 	new PluginBuildScript(
+		str_replace('\\', '/', dirname(__DIR__)),
+		str_replace('\\', '/', __DIR__),
 		'menus',
 		'search',
 		'plg_search_menus',
-		str_replace('\\', '/', dirname(__DIR__)),
-		str_replace('\\', '/', __DIR__),
+		'SharkyKZ',
 		'Search - Menu Items',
 		'Plugin for searching menu items in frontend.',
 		'(4\.|3\.([89]|10))',
 		'5.3.10',
-		'SharkyKZ',
 	)
 )->build();
